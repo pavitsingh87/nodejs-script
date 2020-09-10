8970bbb6d2dde121ab1353d0efab7c840653b0f7
Pavit Singh Makkar <C00302@hsc.com>
2020-09-10 21:50:33 +0530

    first commit

---
+++ routes.js
@@ -0,0 +1,33 @@
+const requestHandler = (req,res)=>{
+    const url = req.url;
+    const method = req.method;
+    if(url==="/")
+    {
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+    }
+    if(url==="/create-user" && method==="POST")
+    {
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', ()=> {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            console.log(message);
+            res.write('<html>');
+            res.write('<head><title>My First Page</title></head>');
+            res.write('<body>'+message+'</body>');
+            res.write('</html>');
+            return res.end();
+          
+        });
+    }
+};
+
+module.exports=requestHandler;
